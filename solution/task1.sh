e7fb91c8bf5c92042c8ad26ced1ef286033e8afe
w1c1y1 <akulov.max3003@gmail.com>
2025-09-09 19:35:22 +0700

    Update2

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,8 +1,8 @@
-cd data
+
 mkdir archive-tmp
-tar -xf archive-part1.tar -C ./archive-tmp
-unzip archive-part2.zip -d ./archive-tmp
-tar -czvf archive-combined.tar.gz ./archive-tmp
-rm -r archive-tmp
+tar -xf ./data/archive-part1.tar -C ./archive-tmp
+unzip -u ./data/archive-part2.zip -d ./archive-tmp
+tar -czf archive-combined.tar.gz ./archive-tmp
+rm -rf archive-tmp
 
 
