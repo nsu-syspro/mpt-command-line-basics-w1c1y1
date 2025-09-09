dfa655dfa324bb4bb8d890832d0123bcf3cb78ce
w1c1y1 <akulov.max3003@gmail.com>
2025-09-09 19:54:21 +0700

    Update4

--- solution/task1.sh
+++ solution/task1.sh
@@ -2,7 +2,7 @@ cd data
 mkdir archive-tmp
 tar -xf ./archive-part1.tar -C ./archive-tmp
 unzip -u ./archive-part2.zip -d ./archive-tmp
-tar -czf archive-combined.tar.gz ./archive-tmp
+tar -czf archive-combined.tar.gz ./archive-tmp/some
 rm -rf archive-tmp
 
 
